2b2e135635257df0f344ac1206df0fdacb326bab
Alec L. Robitaille <robit.alec@gmail.com>
2022-07-28 06:27:55 -0700

    targets render and targets all

--- _targets.R
+++ _targets.R
@@ -9,3 +9,15 @@ lapply(dir('R', '*.R', full.names = TRUE), source)
 
 
 
+# Targets: render ---------------------------------------------------------
+targets_render <- c(
+
+)
+
+
+
+
+# Targets: all ------------------------------------------------------------
+# Automatically grab all the 'targets_*' lists above
+lapply(grep('targets', ls(), value = TRUE), get)
+
