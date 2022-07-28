8e6e7328070d2df5207a3f671c2a267dd06a8dd6
Alec L. Robitaille <robit.alec@gmail.com>
2022-07-28 10:42:22 -0700

    add render lit base

--- _targets.R
+++ _targets.R
@@ -12,7 +12,8 @@ lapply(dir('R', '*.R', full.names = TRUE), source)
 # Targets: render ---------------------------------------------------------
 targets_render <- c(
 	tar_quarto(notes, 'docs/notes.qmd'),
-	tar_quarto(slides, 'slides/slides.qmd')
+	tar_quarto(slides, 'slides/slides.qmd'),
+	tar_quarto(lit_base, 'lit')
 )
 
 
