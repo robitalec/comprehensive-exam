e052e2948c43092bf461a7cdc9cfb0e567231de6
Alec L. Robitaille <robit.alec@gmail.com>
2022-08-03 05:16:21 -0700

    write separate bibs

--- R/parse_bib_to_md_pages.R
+++ R/parse_bib_to_md_pages.R
@@ -2,7 +2,9 @@ parse_bib_to_md_pages <- function(folder = file.path('bib', 'searches')) {
 	bibs <- dir(folder, 'bib', full.names = TRUE)
 	lapply(bibs, function(x) dir.create(xfun::sans_ext(bibs)))
 
+	DT <- as.data.table(bib2df::bib2df(file))
 
+	DT[, writeLines(bib2df::df2bib(.SD), paste0(.BY[[1]], '.bib')), by = UNIQUE.ID]
 	file <- 'test.bib'
 	system(paste('pandoc', file, '-s -f biblatex -t markdown'))
 
