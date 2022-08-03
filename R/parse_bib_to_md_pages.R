9b8fbd600fa75492e0e67105351e3c64fe4cc6af
Alec L. Robitaille <robit.alec@gmail.com>
2022-08-03 05:36:11 -0700

    fix write bib for each

--- R/parse_bib_to_md_pages.R
+++ R/parse_bib_to_md_pages.R
@@ -1,14 +1,17 @@
 parse_bib_to_md_pages <- function(bib) {
-	folder <- xfun::sans_ext(bibs)
+	folder <- xfun::sans_ext(bib)
 	dir.create(folder)
 
 	DT <- as.data.table(bib2df::bib2df(bib))
 
-	DT[, writeLines(bib2df::df2bib(.SD),
-									file.path(folder, paste0(.BY[[1]], '.bib'))),
-		 by = UNIQUE.ID]
+	DT[, {
+		p <- file.path(folder,
+									 paste0(gsub('-', '_', tstrsplit(AUTHOR[[1]][[1]], ',')[[1]]),
+									 			 '_', YEAR, '.bib'))
+		writeLines(bib2df::df2bib(.SD), p)
+		}, by = UNIQUE.ID]
 
-	lapply(dir(folder), function(file) {
+	lapply(dir(folder, full.names = TRUE), function(file) {
 		system(paste('pandoc', file, '-s -f biblatex -t markdown'))
 	})
 }
