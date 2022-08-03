9feea1cc9b5321bc31f75fe8919214da12a5e8bd
Alec L. Robitaille <robit.alec@gmail.com>
2022-08-03 05:10:07 -0700

    dir create

--- R/parse_bib_to_md_pages.R
+++ R/parse_bib_to_md_pages.R
@@ -1,4 +1,8 @@
 parse_bib_to_md_pages <- function(folder = file.path('bib', 'searches')) {
+	bibs <- xfun::sans_ext(dir(folder, 'bib', full.names = TRUE))
+	lapply(bibs, dir.create)
+
+
 	file <- 'test.bib'
 	system(paste('pandoc', file, '-s -f biblatex -t markdown'))
 
