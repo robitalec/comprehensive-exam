331b40eace333e661dd929eca30ea56e4f524dd0
Alec L. Robitaille <robit.alec@gmail.com>
2022-08-03 04:13:04 -0700

    add targets graphviz

--- _targets.R
+++ _targets.R
@@ -9,6 +9,14 @@ lapply(dir('R', '*.R', full.names = TRUE), source)
 
 
 
+
+# Targets: graphviz -------------------------------------------------------
+targets_graphviz <- c(
+	tar_target(why_in_space, DiagrammeR::grViz('gv/why-space-use.gv'))
+)
+
+
+
 # Targets: render ---------------------------------------------------------
 targets_render <- c(
 	tar_quarto(notes, 'manuscript/notes.qmd'),
