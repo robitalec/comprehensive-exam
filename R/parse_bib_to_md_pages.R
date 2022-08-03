f96afc02773ef68315a0f3bb11319631d9214d21
Alec L. Robitaille <robit.alec@gmail.com>
2022-08-03 05:23:16 -0700

    for each bib instead

--- R/parse_bib_to_md_pages.R
+++ R/parse_bib_to_md_pages.R
@@ -1,11 +1,14 @@
-parse_bib_to_md_pages <- function(folder = file.path('bib', 'searches')) {
-	bibs <- dir(folder, 'bib', full.names = TRUE)
-	lapply(bibs, function(x) dir.create(xfun::sans_ext(bibs)))
+parse_bib_to_md_pages <- function(bib) {
+	folder <- xfun::sans_ext(bibs)
+	dir.create(folder)
 
-	DT <- as.data.table(bib2df::bib2df(file))
+	DT <- as.data.table(bib2df::bib2df(bib))
 
-	DT[, writeLines(bib2df::df2bib(.SD), paste0(.BY[[1]], '.bib')), by = UNIQUE.ID]
-	file <- 'test.bib'
-	system(paste('pandoc', file, '-s -f biblatex -t markdown'))
+	DT[, writeLines(bib2df::df2bib(.SD),
+									file.path(folder, paste0(.BY[[1]], '.bib'))),
+		 by = UNIQUE.ID]
 
+	lapply(dir(folder), function(file) {
+		system(paste('pandoc', file, '-s -f biblatex -t markdown'))
+	})
 }
