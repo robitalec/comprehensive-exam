349a6c93ca5c1d3f731738c5026b2338b6449114
Alec L. Robitaille <robit.alec@gmail.com>
2023-03-16 15:58:42 -0300

    fix path

--- _targets.R
+++ _targets.R
@@ -26,7 +26,7 @@ targets_drive  <- c(
 	tar_target(
 		literature,
 		drive_get('literature-comprehensive-exam') |>
-			read_sheet('producer-scrounger-1970-2023_wos'),
+			read_sheet('producer-scrounger-wos-scopus-bioab'),
 		cue = tar_cue('always')
 	),
 	tar_target(
