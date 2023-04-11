47942e09273449747e6269a00f2821b94a982440
Alec L. Robitaille <robit.alec@gmail.com>
2023-04-11 11:01:58 -0300

    scrounger avail

--- scripts/loc-paths.R
+++ scripts/loc-paths.R
@@ -3,6 +3,7 @@ library(ggplot2)
 
 
 
+# Producer ----------------------------------------------------------------
 DT_ed <- CJ(id = 'A', obs = seq.int(3), rdm = seq.int(5))
 
 DT_pt <- CJ(id = 'A', obs = seq.int(3))
@@ -19,6 +20,31 @@ ggplot(DT_pt_rdm) +
 	geom_line(aes(x, y), linewidth = 1)  +
 	theme_void()
 
+
+
+
+# Scrounger ---------------------------------------------------------------
+DT_id <- CJ(id = c('A'), pr_id = c('C', 'D', 'E'), obs = seq.int(1))
+DT_id[, c('x', 'y') := .(c(-1, 0.2, 1), c(1, -0.5, 0.4))]
+
+
+DT_sc <- CJ(id = c('A'), obs = seq.int(1))
+DT_sc[, c('x', 'y') := .(0, 0)]
+
+
+
+ggplot(DT_sc[DT_id, on = 'id']) +
+	geom_point(aes(x, y), size = 2) +
+	geom_point(aes(i.x, i.y, color = pr_id), size = 2) +
+	geom_segment(aes(x = x, y = y, xend = i.x, yend = i.y),
+							 size = 0.2, arrow = arrow(length = unit(0.05, 'inches'))) +
+	# geom_line(aes(x, y), linewidth = 1)  +
+	theme_void()
+
+
+# Conclusion - individuals can choose strategy
+# So might survey conspecifics, then opt to produce
+# DT_sc[, c('x_prod', 'y_prod') := .(x + rnorm(.N, sd = 0.5), y + rnorm(.N, sd = 0.5))]
 
 
 
